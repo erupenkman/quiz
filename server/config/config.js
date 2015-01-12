a108cdbd1b62a0fb215c338440e0953e0b3f07c4
Eru Penkman <eru.penkman@gmail.com>
2015-01-12 19:46:35 +1000

    env for google login

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ exports.FACEBOOK_APP_ID = '340368379503082';
 exports.FACEBOOK_APP_SECRET = '473253ce50a45945cc22bcd219912360';
 exports.FACEBOOK_CALLBACK_URL = 'http://127.0.0.1:8080/auth/facebook/callback';
 
-config.GOOGLE_RETURN_URL = process.env.GOOGLE_RETURN_URL || 'http://127.0.0.1:8080/auth/google/return';
-config.GOOGLE_REALM = process.env.GOOGLE_REALM || 'http://127.0.0.1:8080/';
+exports.GOOGLE_RETURN_URL = process.env.GOOGLE_RETURN_URL || 'http://127.0.0.1:8080/auth/google/return';
+exports.GOOGLE_REALM = process.env.GOOGLE_REALM || 'http://127.0.0.1:8080/';
 
 exports.MONGO_URL = process.env.MONGOLAB_URI || 'mongodb://localhost/health-quiz';
